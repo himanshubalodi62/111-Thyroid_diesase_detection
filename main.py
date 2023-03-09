5f8016152ec2babf5edc89b2999b753430972261
himanshubalodi62 <himanshubalodi62@gmail.com>
2023-03-09 19:50:50 +0530

    added batch prediction

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ file_path= "/config/workspace/hypothyroid.csv"
 
 
 if __name__ == "__main__":
-     try:
-         start_training_pipeline()
-         #output_file = start_batch_prediction(input_file_path=file_path)
-         #print(output_file)
+    try:
+        start_training_pipeline()
+        output_file = start_batch_prediction(input_file_path=file_path)
+        print(output_file)
 
-     except Exception as e:
+    except Exception as e:
           print(e)
