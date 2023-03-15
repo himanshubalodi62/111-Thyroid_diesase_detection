f0030f98f08e4f807916fee0608ce70aa050aeeb
himanshubalodi62 <himanshubalodi62@gmail.com>
2023-03-15 12:22:11 +0530

    added the file

--- batch_predicition.py
+++ batch_predicition.py
@@ -1,11 +1,11 @@
 from thyroid.pipeline.training_pipeline import start_training_pipeline
 from thyroid.pipeline.batch_prediction import start_batch_prediction
 
-file_path="/config/workspace/hypothyroid.csv"
-print(__name__)
+input_file_path = "/config/workspace/hypothyroid.csv"
+
 if __name__=="__main__":
     try:
-        output_file = start_batch_prediction(input_file_path=file_path)
+        output_file = start_batch_prediction(input_file_path=input_file_path)
         print(output_file)
     except Exception as e:
         print(e)
--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 from thyroid.pipeline.training_pipeline import start_training_pipeline
 
 
-file_path="/config/workspace/hypothyroid.csv"
-print(__name__)
+input_file_path="/config/workspace/hypothyroid.csv"
+
 if __name__=="__main__":
     try:
         start_training_pipeline()
